5d7a4eda0666dbd8e95fa69deb964c3714c171cd
longvh0599 <vulong051999@gmail.com>
2020-12-20 22:57:44 +0700

    111

--- book-tour/admin/statistic.php
+++ book-tour/admin/statistic.php
@@ -1,31 +1,33 @@
 <?php
-if(!defined('TEMPLATE')){
-	die('Bạn không có quyền truy cập vào file này !');
+if (!defined('TEMPLATE')) {
+    die('Bạn không có quyền truy cập vào file này !');
 }
 ?>
-<div class="col-sm-2 col-lg-1 main">			
+<div class="col-sm-2 col-lg-1 main">
     <div class="row">
         <ol class="breadcrumb">
             <li><a href="#"><img class="icon-img" src="images/icon/home.png"></a></li>
             <li class="active">Trang chủ quản trị</li>
         </ol>
-    </div><!--/.row-->
-    
+    </div>
+    <!--/.row-->
+
     <div class="row">
         <div class="col-lg-4">
             <h1 class="page-header">Trang chủ quản trị</h1>
         </div>
-    </div><!--/.row-->
-    
+    </div>
+    <!--/.row-->
+
     <div class="row">
         <div class=" col-md-2 col-lg-1">
             <div class="panel panel-blue panel-widget ">
                 <div class="row no-padding">
                     <div class="col-sm-3 col-lg-5 widget-left">
-                        <svg class="glyph stroked bag"><use xlink:href="#stroked-bag"></use></svg>
+                        <img class="simple" src="images/icon/cart.png">
                     </div>
                     <div class="col-sm-9 col-lg-7 widget-right">
-                        <div class="large">120</div>
+                        <div class="large">12</div>
                         <div class="text-muted">Sản Phẩm</div>
                     </div>
                 </div>
@@ -35,10 +37,10 @@ if(!defined('TEMPLATE')){
             <div class="panel panel-orange panel-widget">
                 <div class="row no-padding">
                     <div class="col-sm-3 col-lg-5 widget-left">
-                        <svg class="glyph stroked empty-message"><use xlink:href="#stroked-empty-message"></use></svg>
+                    <img class="simple" src="images/icon/comment.png">
                     </div>
                     <div class="col-sm-9 col-lg-7 widget-right">
-                        <div class="large">52</div>
+                        <div class="large">20</div>
                         <div class="text-muted">Bình Luận</div>
                     </div>
                 </div>
@@ -48,7 +50,7 @@ if(!defined('TEMPLATE')){
             <div class="panel panel-teal panel-widget">
                 <div class="row no-padding">
                     <div class="col-sm-3 col-lg-5 widget-left">
-                        <svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg>
+                    <img class="simple" src="images/icon/user.png">
                     </div>
                     <div class="col-sm-9 col-lg-7 widget-right">
                         <div class="large">24</div>
@@ -57,5 +59,7 @@ if(!defined('TEMPLATE')){
                 </div>
             </div>
         </div>
-    </div><!--/.row-->
-</div>	<!--/.main-->
+    </div>
+    <!--/.row-->
+</div>
+<!--/.main-->
